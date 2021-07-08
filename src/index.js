d9cfa37e08af682ba5bd95e8578b289fb972b227
sumansuriya <sumanprince12345@gmail.com>
2021-07-08 17:55:22 +0530

    commit 4

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+
 import "bootstrap/dist/css/bootstrap.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
